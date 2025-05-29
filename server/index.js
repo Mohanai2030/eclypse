e249e81cd6e3978cb415980629225c1bee7fb777
Mohana Krishnan <mohanakrishnanv18@gmail.com>
2025-05-30 00:08:43 +0530

    updated cors to deployedorigin

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const app = express()
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://eclypse1.vercel.app',
 };
 
 app.use(cors(corsOptions));
